ad73882212b574f0db8b589139c95a131b2a50ed
NatalyCortez <flyingchair_pe12@hotmail.com>
2017-11-09 23:19:26 -0500

    subiendo modificaciones

--- js/app.js
+++ js/app.js
@@ -19,25 +19,31 @@ function isValidCard(number) {
     arrayNumber[i] = number[i];
   }
   newArray = arrayNumber.reverse();
-
+  var sum = 0 ;
+  var numberSum = 0 ;
+  //  empezamos a recorrer el array posicion por posicion
   for (var j = 1; j < newArray.length; j++) {
-    var result;
+    //  si el indice de la posicion es par y distinto de cero se hara lo siguiente
     if (j % 2 === 0 && j !== 0) {//  "numero"%2
+      //  duplicaremos el numero que esta en la posicion par por dos
       var duplicate = newArray[j] * 2;//  10 tipo number
-
+      //  si el doble del numero es mayor o igual a diez
       if (duplicate >= 10) {
+        //  declaro una variable que almacene el valor duplicado y lo convierta a string
         var newDuplicate = duplicate.toString();
-        var sum = '';
+        //  declaro un array vacio para guardar las cifras del numero duplicado
+        var cipherSum = [];
+        //  recorro el numero duplicado de tipo string
         for (var x = 0;x < newDuplicate.length;x++) {
-          sum[x] = parseInt(newDuplicate[x]);
+          //  convierto las cifras del numero de tipo string a number y agrego al array sum cada cifra
+          cipherSum[x] = parseInt(newDuplicate[x]);
         }
-
-        sum = sum[0] + sum[1];
-        result = sum;
+        cipherSum = cipherSum[0] + cipherSum[1];
       }
+      sum += cipherSum;
     } else {
-
-
+      numberSum += parseInt(newArray[j]);
     }
   }
+  var totalSum = (sum + numberSum) ;
 }
