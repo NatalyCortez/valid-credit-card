01bfcc2b29f12229eddfb78678d72af594ed61fa
NatalyCortez <flyingchair_pe12@hotmail.com>
2017-11-13 15:33:22 -0500

    subiendo cambios

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 do {
-  var number = prompt('Ingrese el número de su tarjeta :');// number=45588855(string)
+  //  le pedimos al usuario su numero de tarjeta.
+  var number = prompt('Ingrese el número de su tarjeta : ');
+  //  si el usuario da click en "cancelar" emergera una ventana con un mensaje.
   if (number === null) {
     alert('Hasta luego,regrese pronto!');
     break;
+  //  si el usuario ingresa informacion el programa desarrollara la funcion isValidCard.
   } else if (number !== '') {
     var validation = isValidCard(number);
-    alert('El nùmero de tarjeta es :' + validation);
+    alert(validation);
   } else {
 
   }
+  //  se repitira los pasos indicados arriba mientras el usuario no ingrese nada.
 } while (!number);
 
 
@@ -18,32 +22,42 @@ function isValidCard(number) {
   for (var i = 0; i < number.length; i++) {
     arrayNumber[i] = number[i];
   }
-  newArray = arrayNumber.reverse();
-  var sum = 0 ;
+  arrayNumber.reverse();
   var numberSum = 0 ;
-  //  empezamos a recorrer el array posicion por posicion
-  for (var j = 1; j < newArray.length; j++) {
-    //  si el indice de la posicion es par y distinto de cero se hara lo siguiente
-    if (j % 2 === 0 && j !== 0) {//  "numero"%2
-      //  duplicaremos el numero que esta en la posicion par por dos
-      var duplicate = newArray[j] * 2;//  10 tipo number
-      //  si el doble del numero es mayor o igual a diez
+  //  empezamos a recorrer el array posicion por posicion.
+  for (var j = 0; j < arrayNumber.length; j++) {
+    //  si el indice de la posicion es par y distinto de cero se hara lo siguiente.
+    if (j % 2 !== 0) {
+    //  duplicaremos el numero que esta en la posicion par por dos.
+      var duplicate = arrayNumber[j] * 2;
+      //  si el doble del numero es mayor o igual a diez.
       if (duplicate >= 10) {
-        //  declaro una variable que almacene el valor duplicado y lo convierta a string
+        //  declaro una variable que almacene el valor duplicado y lo convierta a string.
         var newDuplicate = duplicate.toString();
-        //  declaro un array vacio para guardar las cifras del numero duplicado
+        //  declaro un array vacio para guardar las cifras del numero duplicado.
         var cipherSum = [];
-        //  recorro el numero duplicado de tipo string
+        //  recorro el numero duplicado de tipo string.
         for (var x = 0;x < newDuplicate.length;x++) {
-          //  convierto las cifras del numero de tipo string a number y agrego al array sum cada cifra
+          //  convierto las cifras del numero de tipo string a number y agrego al array cipherSum cada cifra.
           cipherSum[x] = parseInt(newDuplicate[x]);
         }
         cipherSum = cipherSum[0] + cipherSum[1];
+        numberSum += cipherSum;
+        //  si el doble del numero es menor a diez.
+      } else {
+        numberSum += parseInt(duplicate);
       }
-      sum += cipherSum;
+      //  si el indice de la posicion es impar e igual a cero se hara lo siguiente.
     } else {
-      numberSum += parseInt(newArray[j]);
+      numberSum += parseInt(arrayNumber[j]);
     }
   }
-  var totalSum = (sum + numberSum) ;
+  //  verificamos si la suma de los numeros es multiplo de 10.
+  if (numberSum % 10 === 0) {
+    //  si el resultado es verdadero retornamos un mensaje que lo indique.
+    return 'Tarjeta de crédito válida.';
+  } else {
+    //  si el resultado es falso retornamos un mensaje que lo indique.
+    return 'Tarjeta de crédito inválida.';
+  }
 }
